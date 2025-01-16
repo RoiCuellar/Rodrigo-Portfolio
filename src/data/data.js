34e29f09fac3daf64b413e0aa3146616ff930912
RoiCuellar <rodrigo.cuellaralons@ucalgary.ca>
2025-01-16 13:30:10 -0700

    Updated Portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,12 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
-    bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
+    name: "Rodrigo Cuellar Alonso",
+    bio: "Undergraduate Student at University of Calgary.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
-        linkedin: "https://www.linkedin.com/",
-        github: "https://www.github.com/",
+        email: "rodrigo.cuellaralons@ucagary.ca",
+        phone: "+15878372440",
+        github: "https://github.com/RoiCuellar",
     }
 }
 
