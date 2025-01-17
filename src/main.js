d377087fe34a422ed1f0cacb672157b0453a720f
RoiCuellar <rodrigo.cuellaralons@ucalgary.ca>
2025-01-17 00:30:07 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -102,6 +102,6 @@ document.querySelector("#app").innerHTML = `
 `;
 
 // Create three.js scenes for each
-createThreeScene("#model1", "/3DModels/project1/cube.obj");
+createThreeScene("#model1", "/3DModels/project1/final.jpg");
 createThreeScene("#model2", "/3DModels/project2/tree.obj");
 createThreeScene("#model3", "/3DModels/project3/cottage.obj");
