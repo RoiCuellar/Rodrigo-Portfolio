cd3d1e385fe6c6f68c1f649338be0529a5c998d8
RoiCuellar <rodrigo.cuellaralons@ucalgary.ca>
2025-01-18 10:58:33 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -38,12 +38,7 @@ document.querySelector("#app").innerHTML = `
         </div>
         <div id="images-description">
           <div id="images">
-            ${collages
-              .map(
-                (collage, index) => `<img src="${collage}" alt="image${index + 1}" />`
-              )
-              .join("")}
-          </div>
+            
           <h4 id="description"> Creating this cadavre exquisite was a new and funny experience for me. I started with a exploration of the concept and various styles/methods before deciding to base mine on a city I find similar to Las Vegas. I focused on removing irrelevant elements and replacing it for stuff that resonate with me, like ariplanes and trains for travel, a roller coaster and ferris wheel for amusement, towering for amazing skyscrapers, and beautifil moon, etc. This process taught me how to merge diverse personal elements into a cohesive base of images, but more important, it challenged me to use codes for creating a portfolio, this experience was stressful and rewarding. Finally, I discovered that many of my personal interests are interconnected and could really coexist in one space. </h4>
         </div>
       </div>
