ca88e7eccbe8787df091dfc1e133aef4b0b6b0dd
RoiCuellar <rodrigo.cuellaralons@ucalgary.ca>
2025-01-18 17:51:46 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import "./style.css";
 import data from "./data/data";
 import { createThreeScene } from "./threeScene";
 
+  const collages = [
+    "./treeDrawings/tree1.jpg",
+    "./treeDrawings/tree2.jpg",
+    "./treeDrawings/tree3.jpg",
+    "./treeDrawings/tree4.jpg",
+  ];
+
+  const series = [
+    "./treeDrawings/tree1.jpg",
+    "./treeDrawings/tree2.jpg",
+    "./treeDrawings/tree3.jpg",
+    "./treeDrawings/tree4.jpg",
+  ];
+
 const sources = [
   "./images/casino.jpg",
   "./images/moon.jpg",
@@ -23,18 +37,29 @@ document.querySelector("#app").innerHTML = `
           <img src="./final/final.jpg" id="model1" alt="1st Section Image" />
         </div>
         <div id="images-description">
-          <div id="images">
-            ${collages
-              .map(
-                (collage, index) => `<img src="${collage}" alt="image${index + 1}" />`
-              )
-              .join("")}
+         <div id="images">
+            <a href="https://ca.pinterest.com/pin/950611433849087355/" target="_blank">
+              <img src="./images/casino.jpg" alt="casino" />
+            </a>
+            <a href="https://ca.pinterest.com/pin/619667230005328777/" target="_blank">
+              <img src="./images/moon.jpg" alt="moon" />
+            </a>
+            <a href="https://ca.pinterest.com/pin/950611433849093575/" target="_blank">
+              <img src="./images/wheel.jpg" alt="wheel" />
+            </a>
+            <a href="https://ca.pinterest.com/pin/950611433849093736/" target="_blank">
+              <img src="./images/train.jpg" alt="train" />
+            </a>
           </div>
-          <h4 id="description"> Creating this cadavre exquisite was a new and funny experience for me. I started with a exploration of the concept and various styles/methods before deciding to base mine on a city I find similar to Las Vegas. I focused on removing irrelevant elements and replacing it for stuff that resonate with me, like ariplanes and trains for travel, a roller coaster and ferris wheel for amusement, towering for amazing skyscrapers, and beautifil moon, etc. This process taught me how to merge diverse personal elements into a cohesive base of images, but more important, it challenged me to use codes for creating a portfolio, this experience was stressful and rewarding. Finally, I discovered that many of my personal interests are interconnected and could really coexist in one space. </h4>
+          <h4 id="description">
+            Image 1. Alisa, A. (n.d.). Jackpot Beefee casino [Image]. Pinterest. https://ca.pinterest.com/pin/950611433849087355/. <br> 
+            Image 2. Dreamstime. (2024). Vector Full Moon [Image]. Pinterest. https://ca.pinterest.com/pin/619667230005328777/. <br>
+            Image 3. Allana. (2024). [Image]. Pinterest. https://ca.pinterest.com/pin/950611433849093575/. <br>
+            Image 4. Tetsudo, S. (n.d.). Train [Image]. Pinterest. https://ca.pinterest.com/pin/950611433849093736/.
         </div>
       </div>
 
-      ${/*<div id="project-row">
+      <div id="project-row">
       2nd Section
         <div class="three-model">
           <div id="model2"></div>
@@ -50,9 +75,7 @@ document.querySelector("#app").innerHTML = `
           <h4 id="description">A 3D tree model is a geometric representation of a tree, featuring a trunk, branches, and foliage. The trunk is typically cylindrical, with textured bark, while the branches extend outward to support leaves or a canopy. It may include roots or flowers for added detail.</h4>
         </div>
       </div>
-      */""}
 
-      ${/*
       <div id="project-row">
       3rd Section
         <div class="three-model">
@@ -81,7 +104,6 @@ document.querySelector("#app").innerHTML = `
           </h4>
           </div>
       </div>
-      */""}
     </section>
 
     <ul id="footer-items">
@@ -99,5 +121,5 @@ document.querySelector("#app").innerHTML = `
 
 // Create three.js scenes for each
 createThreeScene("#model1", "/3DModels/project1/final.jpg");
-//createThreeScene("#model2", "/3DModels/project2/tree.obj");
-//createThreeScene("#model3", "/3DModels/project3/cottage.obj");
+createThreeScene("#model2", "/3DModels/project2/tree.obj");
+createThreeScene("#model3", "/3DModels/project3/cottage.obj");
